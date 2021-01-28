be71375dd693b5cb49384ec565578b2e40388642
digitalparesh <prshnandaniya@gmail.com>
2021-01-28 12:36:36 +0530

    updated home.page.ts file

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from "@angular/fire/firestore";
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: "app-home",
@@ -11,31 +12,59 @@ import {
 })
 export class HomePage {
   private itemsCollection: AngularFirestoreCollection;
-  item = {
-    value: "",
-  };
+  item;
   itemArr = [];
-  constructor(private afs: AngularFirestore) {
-    this.itemsCollection = afs.collection("item");
-    this.itemsCollection.valueChanges().subscribe((item) => {
-      // this.itemArr = item;
-      // console.log(this.itemArr);
-    });
+  constructor(
+    private afs: AngularFirestore,
+    public alertController: AlertController
+  ) {
+    // this.itemsCollection = afs.collection("item");
+    // this.itemsCollection.valueChanges().subscribe((item) => {
+    //   this.itemArr = item;
+    // });
     this.itemsCollection.snapshotChanges().subscribe((data) => {
       data.map((item) => {
         console.log({ [item.payload.doc.id]: item.payload.doc.data() });
       });
     });
+    this.afs
+      .collection("item")
+      .doc("itemdoc")
+      .valueChanges()
+      .subscribe((item) => {
+        this.itemArr = item["value"];
+        // console.log(item);
+      });
   }
 
   addData() {
-    console.log(this.item.value);
-    // this.afs.collection("item").doc("Hello").update(this.item);
-    this.afs.collection("item").add(this.item);
-    this.item.value = "";
+    // console.log(this.item.value);
+    // // this.afs.collection("item").doc("Hello").update(this.item);
+    // this.afs.collection("item").add(this.item);
   }
 
-  deleteItem(index) {
-    this.afs.collection("item").doc(index).delete();
+  deleteItem(id) {}
+
+  updateItem(id) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: "my-custom-class",
+      header: "Update Item",
+      subHeader: "",
+      inputs: [
+        {
+          name: "item",
+          type: "text",
+
+          placeholder: "update item",
+        },
+      ],
+      message: "This is an alert message.",
+      buttons: ["Update"],
+      mode: "ios",
+    });
+
+    await alert.present();
   }
 }
