39a1202c179be91f0b718ccf29b2e0f46dfe0fe4
digitalparesh <prshnandaniya@gmail.com>
2021-01-28 15:40:17 +0530

    fixed build issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDm_g9NdhAA7RLEee94AwpCSwRjnnJ0zXU",
+    authDomain: "ionic-crud-f35be.firebaseapp.com",
+    projectId: "ionic-crud-f35be",
+    storageBucket: "ionic-crud-f35be.appspot.com",
+    messagingSenderId: "88270518937",
+    appId: "1:88270518937:web:1a691b554c0a3a30d65608",
+  },
 };
